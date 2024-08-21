9df3b5b80aee0082b1c8eada5247e66d2bf9ed96
William Schultz <will627@comcast.net>
2024-08-21 14:22:41 -0400

    Fix quoting

--- MultiShardTxn/check.py
+++ MultiShardTxn/check.py
@@ -72,7 +72,7 @@ fout.close()
 # Run TLC model checker via command line with subprocess.
 tlc_jar=args.tlc_jar
 tlc_args = args.tlc_args
-args = ["java", "-cp", tlc_jar, "tlc2.TLC"] + [tlc_args] + ["-config", "MultiShardTxn_gen.cfg", "MultiShardTxn"]
+args = ["java", f"-cp {tlc_jar}", "tlc2.TLC"] + [tlc_args] + ["-config", "MultiShardTxn_gen.cfg", "MultiShardTxn"]
 print(args)
 print(" ".join(args))
 ret = subprocess.run(" ".join(args), shell=True)
