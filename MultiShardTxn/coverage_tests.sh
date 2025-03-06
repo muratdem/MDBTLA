3dfdb71efa0580a0a65fc16e351b286a40c4dea7
William Schultz <william.schultz@mongodb.com>
2025-03-06 01:14:49 -0500

    save experiment script

--- MultiShardTxn/coverage_tests.sh
+++ MultiShardTxn/coverage_tests.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-python3 trace.py --compact --constants MTxId "{t1,t2}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct 0.1 --use_json_graph && source copy_test.sh "0.1"
-python3 trace.py --compact --constants MTxId "{t1,t2}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct 0.2 --use_json_graph && source copy_test.sh "0.2"
-python3 trace.py --compact --constants MTxId "{t1,t2}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct 0.3 --use_json_graph && source copy_test.sh "0.3"
-python3 trace.py --compact --constants MTxId "{t1,t2}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct 0.4 --use_json_graph && source copy_test.sh "0.4"
-python3 trace.py --compact --constants MTxId "{t1,t2}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct 0.5 --use_json_graph && source copy_test.sh "0.5"
-python3 trace.py --compact --constants MTxId "{t1,t2}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct 0.6 --use_json_graph && source copy_test.sh "0.6"
+
+# 1 txn.
+for cvg in 0.1 0.2 0.3 0.4 0.5; do
+    python3 trace.py --compact --constants MTxId "{t1}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct $cvg --use_json_graph && source copy_test.sh "coverage $cvg, 1 txns"
+done
+
+# 2 txns.
+for cvg in 0.1 0.2 0.3 0.4 0.5; do
+    python3 trace.py --compact --constants MTxId "{t1,t2}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct $cvg --use_json_graph && source copy_test.sh "coverage $cvg, 2 txns"
+done
+
+
