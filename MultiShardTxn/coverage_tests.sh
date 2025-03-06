f20b65bbaa21f57f9244528fc28d023fb9811317
William Schultz <william.schultz@mongodb.com>
2025-03-06 10:51:07 -0500

    Update coverage test script

--- MultiShardTxn/coverage_tests.sh
+++ MultiShardTxn/coverage_tests.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 # 1 txn.
-for cvg in 0.1 0.2 0.3 0.4 0.5; do
-    python3 trace.py --compact --constants MTxId "{t1}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct $cvg --use_json_graph && source copy_test.sh "coverage $cvg, 1 txns"
+txids="{t1}"
+
+# Generate the graph upfront.
+python3 trace.py --compact --constants MTxId "$txids" Keys "{k1,k2}" Timestamps "{1,2,3}" --generate_only
+for cvg in 0.2 0.4 0.6 0.8 1.0; do
+    python3 trace.py --compact --constants MTxId "$txids" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct $cvg --use_cached_graphs && source copy_test.sh "coverage $cvg, 1 txns"
 done
 
 # 2 txns.
-for cvg in 0.1 0.2 0.3 0.4 0.5; do
-    python3 trace.py --compact --constants MTxId "{t1,t2}" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct $cvg --use_json_graph && source copy_test.sh "coverage $cvg, 2 txns"
+txids="{t1,t2}"
+
+# Generate the graph upfront.
+python3 trace.py --compact --constants MTxId "$txids" Keys "{k1,k2}" Timestamps "{1,2,3}" --generate_only
+for cvg in 0.2 0.4 0.6 0.8 1.0; do
+    python3 trace.py --compact --constants MTxId "$txids" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct $cvg --use_cached_graphs && source copy_test.sh "coverage $cvg, 2 txns"
 done
-
-
