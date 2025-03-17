48e3ae1b80187d404dade5824282c25d4e4f6266
William Schultz <william.schultz@mongodb.com>
2025-03-17 00:09:59 -0400

    Enable symmetry in permissiveness

--- MultiShardTxn/permissiveness.sh
+++ MultiShardTxn/permissiveness.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 ignore_vars="rtxn,rTxnReadTs,rInCommit,rParticipants,rCatalog,shardTxnReqs,shardTxns,shardPreparedTxns,coordCommitVotes,aborted,coordInfo,msgsPrepare,msgsVoteCommit,msgsAbort,msgsCommit,shardOps,catalog,log,txnSnapshots,txnStatus,stableTs,oldestTs,allDurableTs"
 
+# rc=snapshot
+java -cp tla2tools-json-test.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 1 -config MCMultiShardTxn_RC_snapshot.cfg MCMultiShardTxn.tla | tee logout_permissive_rc_snapshot.txt
+
 # rc=local with prepare blocking.
-# java -cp tla2tools-json-test.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 10 -config MCMultiShardTxn_RC_with_prepare_block.cfg MCMultiShardTxn.tla | tee logout_permissive_rc_with_prepare_block.txt
+# java -cp tla2tools-json-test.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 1 -config MCMultiShardTxn_RC_with_prepare_block.cfg MCMultiShardTxn.tla | tee logout_permissive_rc_with_prepare_block.txt
 
 # rc=local with no prepare blocking.
-java -cp tla2tools-json-test.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 10 -config MCMultiShardTxn_RC_no_prepare_block.cfg MCMultiShardTxn.tla  | tee logout_permissive_rc_no_prepare_block.txt
+# java -cp tla2tools-json-test.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 1 -config MCMultiShardTxn_RC_no_prepare_block.cfg MCMultiShardTxn.tla  | tee logout_permissive_rc_no_prepare_block.txt
 
 # rc=local with no prepare blocking or ww.
 # java -cp tla2tools-json-test.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 10 -config MultiShardTxn_RC_no_prepare_block_or_ww.cfg MCMultiShardTxn.tla | tee logout_permissive_rc_no_prepare_block_or_ww.txt
