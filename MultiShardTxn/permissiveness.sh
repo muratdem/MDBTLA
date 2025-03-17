f1b1717ba54951bd6b4d8d803d566e19cef8b971
William Schultz <william.schultz@mongodb.com>
2025-03-16 23:39:16 -0400

    Save permissiveness scripts

--- MultiShardTxn/permissiveness.sh
+++ MultiShardTxn/permissiveness.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-ignore_vars="rtxn,rTxnReadTs,rInCommit,rParticipants,rCatalog,shardTxnReqs,shardTxns,shardPreparedTxns,coordCommitVotes,aborted,coordInfo,msgsPrepare,msgsVoteCommit,msgsAbort,msgsCommit,shardOps,catalog,log,txnSnapshots"
+ignore_vars="rtxn,rTxnReadTs,rInCommit,rParticipants,rCatalog,shardTxnReqs,shardTxns,shardPreparedTxns,coordCommitVotes,aborted,coordInfo,msgsPrepare,msgsVoteCommit,msgsAbort,msgsCommit,shardOps,catalog,log,txnSnapshots,txnStatus,stableTs,oldestTs,allDurableTs"
 
 # rc=local with prepare blocking.
-java -cp tla2tools-json.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 10 -config MultiShardTxn_RC_with_prepare_block.cfg MCMultiShardTxn.tla | tee logout_permissive_rc_with_prepare_block.txt
+# java -cp tla2tools-json-test.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 10 -config MCMultiShardTxn_RC_with_prepare_block.cfg MCMultiShardTxn.tla | tee logout_permissive_rc_with_prepare_block.txt
 
 # rc=local with no prepare blocking.
-java -cp tla2tools-json.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 10 -config MultiShardTxn_RC_no_prepare_block.cfg MCMultiShardTxn.tla  | tee logout_permissive_rc_no_prepare_block.txt
+java -cp tla2tools-json-test.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 10 -config MCMultiShardTxn_RC_no_prepare_block.cfg MCMultiShardTxn.tla  | tee logout_permissive_rc_no_prepare_block.txt
 
 # rc=local with no prepare blocking or ww.
-java -cp tla2tools-json.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 10 -config MultiShardTxn_RC_no_prepare_block_or_ww.cfg MCMultiShardTxn.tla | tee logout_permissive_rc_no_prepare_block_or_ww.txt
+# java -cp tla2tools-json-test.jar tlc2.TLC -cacheStatesIgnoreVars $ignore_vars -cacheStates cache -deadlock -workers 10 -config MultiShardTxn_RC_no_prepare_block_or_ww.cfg MCMultiShardTxn.tla | tee logout_permissive_rc_no_prepare_block_or_ww.txt
 
 # Baseline
-java -cp tla2tools-json.jar tlc2.TLC ClientCentricTests.tla | tee logout_baseline.txt
+# java -cp tla2tools-json.jar tlc2.TLC ClientCentricTests.tla | tee logout_baseline.txt
 
 
 
