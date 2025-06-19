7a8b149f0711f96d022dbd3e19f0a043e2df7a1b
William Schultz <william.schultz@mongodb.com>
2025-06-19 19:45:56 +0000

    Remove old stuff

--- MultiShardTxn/copy_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-tag=$1
-scp test_txn_model_traces.py mongo-dev-workstation:/home/ubuntu/wiredtiger/test/suite/
-ssh mongo-dev-workstation "cd wiredtiger && git add test/suite/test_txn_model_traces.py && git commit -m save"
-evg_cmd="/home/ubuntu/evergreen patch --large -p wiredtiger -t coverage-report-python,generate-coverage-report -v code-statistics -d '$tag' -f -y"
-ssh mongo-dev-workstation "cd wiredtiger && $evg_cmd"
