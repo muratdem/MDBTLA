def46c34a9ad6a9def31639ac631c02d8aaed8bd
William Schultz <william.schultz@mongodb.com>
2025-06-19 19:44:47 +0000

    Update script

--- MultiShardTxn/testrun.sh
+++ MultiShardTxn/testrun.sh
@@ -10,14 +10,17 @@ cvg_pct=$1
 txids="{t1,t2}"
 wt_base_dir="/home/ubuntu/wiredtiger"
 
-# Generate test cases from WiredTiger model.
-jobs=4
-# python3 trace.py --compact --constants MTxId "$txids" Keys "{k1,k2}" Timestamps "{1,2,3}" --generate_only
-cd /home/ubuntu/MDBTLA/MultiShardTxn
+#
+# Generate unit test cases from WiredTiger TLA+ model 
+# via path coverings of state space.
+#
+jobs=6
 python3 testgen.py --parallel_test_split $jobs --compact --constants MTxId "$txids" Keys "{k1,k2}" Timestamps "{1,2,3}" --coverage_pct $cvg_pct 
 cp tests/test_txn_model_traces*.py $wt_base_dir/test/suite
 
+#
 # Run the tests against WiredTiger.
+#
 cd $wt_base_dir/build
 test_list=""
 for i in $(seq 1 $jobs); do
